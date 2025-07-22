cc68b7b710185c9e9e4d32a1196699bd2f29ea16
Marina Zhivotova <mazhivotova@gmail.com>
2025-07-22 18:43:21 +0300

    start doing parsing, add is_valid_input function

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 14:14:53 by mzhivoto          #+#    #+#             */
-/*   Updated: 2025/07/22 16:02:29 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:41:53 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,31 @@
  #include <stdlib.h>
 
  
-
-//  int is_valid_input(int ac, char **av)
-//  {
-// 	int i = 0;
-// 	while (av[i])
-// 	{
-// 		if(av[i]);
-
-// 	}
-//  }
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+ int is_valid_input(char *str)
+ {
+	int i = 0;
+	while (str[i])
+	{
+		if(!ft_isdigit(str[i]))
+		{
+			printf("error found\n");
+			return 0;
+		}
+	}
+	return 1;
+ }
 
 int main(int ac, char **av)
 {
 	(void)ac;
+	
 	//int i = 0;
 	int number_of_philos = 0;
 	int time_to_die = 0;
@@ -40,7 +51,7 @@ int main(int ac, char **av)
 	time_to_eat = atoi(av[3]);
 	time_to_sleep = atoi(av[4]);
 	number_of_how_much_eat = atoi(av[5]);
-
+	is_valid_input(*av);
 	printf("1: %d\n", number_of_philos);
 	printf("2: %d\n", time_to_die);
 	printf("3: %d\n", time_to_eat);
